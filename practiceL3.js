2aeb8965170c49aba25bde10c39d8ca555a1af30
Sanjeevan76 <studyyou40@gmail.com>
2024-06-29 19:47:39 +0530

    lecture 3 practice problem

---
+++ practiceL3.js
@@ -0,0 +1,16 @@
+//practice proble 01
+// for(let i=0;i<=100;i++)
+//     {
+//         if(i%2===0)
+//             {
+//                 console.log(i);
+//             }
+//     }
+
+ // practice problem 02
+//take a name from user and generate username starting with @ and end with user name length
+
+let user = prompt("enter your full name");
+let userName=("@"+user);
+ console.log(userName + user.length);
+
