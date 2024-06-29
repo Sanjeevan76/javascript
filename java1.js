76aef0fa2b0ed11d8a49564c60ba4453fdd5afc5
Sanjeevan76 <studyyou40@gmail.com>
2024-06-29 11:18:13 +0530

    easy

---
+++ java1.js
@@ -0,0 +1,17 @@
+console.log("sanjeevan kumar");
+
+let fullName="sanjeevan kumar";
+console.log(fullName);
+// let fullName="rohit";  //do not declear again let variable
+//we can only modify that variable
+
+fullName="shardha Didi";
+console.log(fullName);
+
+const greatName="shardha khapra mam";
+console.log(greatName);
+
+//we can't re declear and modify that variable when we use const variable type
+
+
+
